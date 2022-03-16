c0509b09501c32d7f6a2fd860bd740cc125ddf10
doctorjaffa <nathanharris1502@gmail.com>
2022-03-16 15:03:08 +0000

    Altered player movement for keyboard input.

--- Colourful Chaos Unity/Assets/Scripts/PlayerMovement.cs
+++ Colourful Chaos Unity/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
+    public float moveSpeed = 3.0f;
+
     private Rigidbody2D physicsBody = null;
 
     // Start is called before the first frame update
@@ -13,12 +15,25 @@ public class PlayerMovement : MonoBehaviour
     {
         physicsBody = GetComponent<Rigidbody2D>();
 
-        physicsBody.velocity = new Vector2(2, 0);
+        //physicsBody.velocity = new Vector2(2, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Ask Unity's Input Manager for the current value of the horizontal axis.
+        //This will be between -1 and 1. 
+        float axisValX = Input.GetAxis("Horizontal");
+        float axisValY = Input.GetAxis("Vertical");
+
+        //Use axes values to set up a new velocity vector.
+        Vector2 newVel = new Vector2(axisValX, axisValY);
+
+        //Scale our velovity based on a speed.
+        //Goes from -speed to +speed.
+        newVel *= moveSpeed;
+
+        //Tell the physics rigidbody to use the new velocity.
+        physicsBody.velocity = newVel;
     }
 }
